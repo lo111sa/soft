d01254de0544442fd3d1820780acfaa8401b0a4f
lo111sa <losaberidze777@yahoo.com>
2023-12-24 18:24:52 +0400

    Home

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -1,15 +1,6 @@
-import React, { useEffect, useState } from "react";
-import Input from "./UI/Input";
-import Select from "./UI/Select";
-import CustomDatePicker from "./UI/CustomDatePicker";
-import { usePatientsStore } from "./../store/patientsStore";
-import { useDoctorsStore } from "./../store/doctorsStore";
-import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined";
-import PaymentOutlinedIcon from "@mui/icons-material/PaymentOutlined";
-import LocalPhoneIcon from "@mui/icons-material/LocalPhone";
-import HouseIcon from "@mui/icons-material/House";
-const Test = () => {
-  return <></>;
-};
+import React from "react";
+
+// Create Document Component
+const Test = () => <></>;
 
 export default Test;
