0be7e7d2de78058f37f98eb4dd86b9dca67f429d
lo111sa <losaberidze777@yahoo.com>
2024-03-20 22:23:32 +0400

    Home

--- src/pages/registry/components/tables/AmbulTable.jsx
+++ src/pages/registry/components/tables/AmbulTable.jsx
@@ -10,7 +10,7 @@ const AmbulTable = () => {
   const modal = useModalStore();
 
   const tableHeader = (
-    <tr className="text-[15px]  sticky top-[-0.5px]  z-10 text-white bg-gradient-to-b from-[#57add8] to-[#2288C4] rounded-lg">
+    <tr className="text-[15px] text-white sticky top-[-0.5px]  z-10  bg-gradient-to-b from-[#57add8] to-[#2288C4] rounded-lg">
       <th scope="col" className="px-6 py-3 rounded-tl-lg">
         N
       </th>
@@ -19,6 +19,9 @@ const AmbulTable = () => {
       </th>
       <th scope="col" className="px-6 py-5 ">
         პაციენტი
+      </th>
+      <th scope="col" className="px-6 py-5 ">
+        ექიმი
       </th>
       <th scope="col" className="px-6 py-5 ">
         ისტორიის ნომერი
@@ -38,39 +41,38 @@ const AmbulTable = () => {
     <table className="w-full text-sm text-left rtl:text-right text-black  cursor-pointer border-separate border-spacing-y-2">
       <thead className="text-xs text-gray-800 bg-gray-50">{tableHeader}</thead>
       <tbody className="">
-        {visits.amb?.length ? (
-          visits.amb.map((item, index) => {
-            return (
-              <tr
-                key={item.id}
-                className={`hover:bg-gray-200 bg-white shadow rounded-lg`}
-                onClick={() =>
-                  modal.open(
-                    <PatientVisitInfo info={item} />,
-                    "პაციენტის ვიზიტების ისტორია"
-                  )
-                }
-              >
-                <td className="px-6 py-4 rounded-s-lg ">{index + 1}</td>
-                <td className="px-6 py-4 ">{item.patientPn}</td>
-                <td className="px-6 py-4  ">{item.patientName}</td>
-                <td className="px-6 py-4  ">{"47586"}</td>
-                <td className="px-6 py-4  ">{formatDate(item.visitTime)}</td>
-                <td className="rounded-e-lg ">
-                  <div className="flex justify-center items-center">
-                    <div
-                      className={`w-4 h-4 p-0 m-0 rounded-full ${
-                        item.status ? `bg-[#30c279]` : `bg-[#f1533e]`
-                      } self-center`}
-                    ></div>
-                  </div>
-                </td>
-              </tr>
-            );
-          })
-        ) : (
-          <p>Loading...</p>
-        )}
+        {visits.amb?.length
+          ? visits.amb.map((item, index) => {
+              return (
+                <tr
+                  key={item.id}
+                  className={`hover:bg-gray-200 bg-white shadow rounded-lg`}
+                  onClick={() =>
+                    modal.open(
+                      <PatientVisitInfo info={item} />,
+                      "პაციენტის ვიზიტების ისტორია"
+                    )
+                  }
+                >
+                  <td className="px-6 py-4 rounded-s-lg ">{index + 1}</td>
+                  <td className="px-6 py-4 ">{item.patientPn}</td>
+                  <td className="px-6 py-4  ">{item.patientName}</td>
+                  <td className="px-6 py-4  ">{item.doctorName}</td>
+                  <td className="px-6 py-4  ">{"47586"}</td>
+                  <td className="px-6 py-4  ">{formatDate(item.visitTime)}</td>
+                  <td className="rounded-e-lg ">
+                    <div className="flex justify-center items-center">
+                      <div
+                        className={`w-4 h-4 p-0 m-0 rounded-full ${
+                          item.status ? `bg-[#30c279]` : `bg-[#f1533e]`
+                        } self-center`}
+                      ></div>
+                    </div>
+                  </td>
+                </tr>
+              );
+            })
+          : null}
       </tbody>
     </table>
   );
