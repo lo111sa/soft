2450e8e3e5ad6dc86b494d0f5c48626830b272f0
lo111sa <losaberidze777@yahoo.com>
2024-03-25 16:23:35 +0400

    Work

--- src/axios.js
+++ src/axios.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 //https://kind-lime-puppy-slip.cyclic.app/api
 const instance = axios.create({
-  baseURL: "http://10.1.10.10:8000/api",
-  // baseURL: "http://94.43.207.20:8000/api",
+  //baseURL: "http://10.1.10.10:8000/api",
+  baseURL: "http://losa.ge:8000/api",
   withCredentials: true,
 });
 
